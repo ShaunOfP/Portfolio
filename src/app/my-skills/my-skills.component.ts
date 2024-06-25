71cbd0e04e9d9749930a0ce8d816754e69986d5e
ShaunOfP <131476113+ShaunOfP@users.noreply.github.com>
2024-06-25 18:29:29 +0200

    added my skills section and design; in progress

--- src/app/my-skills/my-skills.component.ts
+++ src/app/my-skills/my-skills.component.ts
@@ -1,12 +1,52 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-my-skills',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './my-skills.component.html',
   styleUrl: './my-skills.component.scss'
 })
 export class MySkillsComponent {
-
+  iconInfos = [{
+    "path": "icon-angular",
+    "name": "Angular"
+  },
+  {
+    "path": "icon-ts",
+    "name": "TypeScript"
+  },
+  {
+    "path": "icon-js",
+    "name": "JavaScript"
+  },
+  {
+    "path": "icon-html",
+    "name": "HTML"
+  },
+  {
+    "path": "icon-firebase",
+    "name": "Firebase"
+  },
+  {
+    "path": "icon-git",
+    "name": "GIT"
+  },
+  {
+    "path": "icon-css",
+    "name": "CSS"
+  },
+  {
+    "path": "icon-api",
+    "name": "Rest-Api"
+  },
+  {
+    "path": "icon-scrum",
+    "name": "Scrum"
+  },
+  {
+    "path": "icon-mat-design",
+    "name": "Material Design"
+  }];
 }
