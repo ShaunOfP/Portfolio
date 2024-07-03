b6b771391334b39ec38d21ffa57efba18e348a5a
ShaunOfP <131476113+ShaunOfP@users.noreply.github.com>
2024-07-03 17:05:08 +0200

    adding logic to activate header anchor elements on click; in progress

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  line1 = false;
+  line2 = false;
+  line3 = false;
 
+  activateBtn(id: number) {
+    switch (id) {
+      case id = 1:
+        console.log(1);
+        break;
+      case id = 2:
+        console.log(2);
+        break;
+      case id = 3:
+        console.log(3);
+        break;
+    }
+  }
 }
