29a2fb9fa85a48259f04b7eaaa93609878180f0e
ShaunOfP <131476113+ShaunOfP@users.noreply.github.com>
2024-07-04 17:50:31 +0200

    added hover effect on imprint

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  mouseOver: boolean = false;
 
+  mouseOverTrue(){
+    this.mouseOver = true;
+  }
+
+  mouseOverFalse(){
+    this.mouseOver = false;
+  }
 }
