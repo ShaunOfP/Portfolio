8f5244e77fe1a9abc8acbcad348b595f98c3cd28
ShaunOfP <131476113+ShaunOfP@users.noreply.github.com>
2024-07-09 16:55:40 +0200

    added for loop and if condition

--- src/app/landing-page-content/components/projects-portfolio/projects-portfolio.component.ts
+++ src/app/landing-page-content/components/projects-portfolio/projects-portfolio.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { ProjectBlueprintComponent } from './project-blueprint/project-blueprint.component';
 import { ProjectBlueprintMirroredComponent } from './project-blueprint-mirrored/project-blueprint-mirrored.component';
 
-
 @Component({
   selector: 'app-projects-portfolio',
   standalone: true,
@@ -11,5 +10,31 @@ import { ProjectBlueprintMirroredComponent } from './project-blueprint-mirrored/
   styleUrl: './projects-portfolio.component.scss'
 })
 export class ProjectsPortfolioComponent {
-  
+  projects = [
+    {
+      'name': 'Join',
+      'languages': ['HTML', 'CSS', 'JavaScript'],
+      'description': 'Task manager inspired by the Kanban System. Create and organzie tasks using drag and drop function, assign users and categories.',
+    },
+    {
+      'name': 'El Pollo Loco',
+      'languages': ['HTML', 'CSS', 'JavaScript'],
+      'description': 'A simple Jump-and-Run game based on an object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen.',
+    },
+    {
+      'name': 'x',
+      'languages': ['x', 'x', 'x'],
+      'description': 'x',
+    },
+    {
+      'name': 'x',
+      'languages': ['x', 'x', 'x'],
+      'description': 'x',
+    },
+    {
+      'name': 'x',
+      'languages': ['x', 'x', 'x'],
+      'description': 'x',
+    }
+  ];
 }
