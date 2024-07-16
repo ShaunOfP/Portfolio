d5b7b071a051593b009bdcca636c28e515564724
ShaunOfP <131476113+ShaunOfP@users.noreply.github.com>
2024-07-16 10:23:03 +0200

    added style to the contact section, also added privacy-policy and style to header and my skills section

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LandingPageContentComponent } from './landing-page-content/landing-page-content.component'
 import { ImprintComponent } from './imprint/imprint.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
 export const routes: Routes = [
     { path: '', component: LandingPageContentComponent},
-    { path: 'imprint', component: ImprintComponent}
+    { path: 'imprint', component: ImprintComponent},
+    { path: 'privacy-policy', component: PrivacyPolicyComponent}
 ];
