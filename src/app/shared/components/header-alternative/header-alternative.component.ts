99368ff8a5a267386aedf70234994d50ae8ec0ca
ShaunOfP <131476113+ShaunOfP@users.noreply.github.com>
2024-08-17 13:48:59 +0200

    added language switch for the alternative header

--- src/app/shared/components/header-alternative/header-alternative.component.ts
+++ src/app/shared/components/header-alternative/header-alternative.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header-alternative',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header-alternative.component.html',
   styleUrl: './header-alternative.component.scss'
 })
 export class HeaderAlternativeComponent {
+  isInactive: boolean = false;
+  isInactive1: boolean = true;
 
+  activateLanguageIcon(id: string) {
+    switch (id) {
+      case id = "en":
+        this.isInactive = false;
+        this.isInactive1 = true;
+        break;
+      case id = "ger":
+        this.isInactive = true;
+        this.isInactive1 = false;
+        break;
+    }
+  }
 }
