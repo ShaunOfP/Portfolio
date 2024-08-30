5f555b7359f136a2006b30be07460b8f53da0c58
ShaunOfP <131476113+ShaunOfP@users.noreply.github.com>
2024-08-30 10:29:29 +0200

    added animation for projects to fade in when intersecting view

--- src/app/landing-page-content/components/projects-portfolio/projects-portfolio.component.ts
+++ src/app/landing-page-content/components/projects-portfolio/projects-portfolio.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { ProjectBlueprintComponent } from './project-blueprint/project-blueprint.component';
 import { ProjectBlueprintMirroredComponent } from './project-blueprint-mirrored/project-blueprint-mirrored.component';
 import { Item } from '../../../../models/item.model';
@@ -11,7 +11,7 @@ import { TranslateModule } from '@ngx-translate/core';
   templateUrl: './projects-portfolio.component.html',
   styleUrl: './projects-portfolio.component.scss'
 })
-export class ProjectsPortfolioComponent {
+export class ProjectsPortfolioComponent implements AfterViewInit {
   projects: Item[] = [
     {
       'name': 'Join',
@@ -38,4 +38,34 @@ export class ProjectsPortfolioComponent {
       'githubPath': 'https://github.com/ShaunOfP/Pok-dex'
     },
   ];
+
+  elementsArray: Element[] = [];
+  observer!: IntersectionObserver;
+
+  ngAfterViewInit(): void {
+    const elements = document.querySelectorAll('.hidden');
+    elements.forEach((project) => {
+      this.elementsArray.push(project);
+    });
+    this.initializeObserver();
+    this.addObserver();
+  }
+
+
+  initializeObserver() {
+    this.observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('show');
+        }
+      });
+    });
+  }
+
+
+  addObserver() {
+    this.elementsArray.forEach((project) => {
+      this.observer.observe(project);
+    });
+  }
 }
